6c172469e7b28481c76517bdcea3c934251edfa7
filipgrabovac-netgen <filip.grabovac@netgen.io>
2026-01-01 21:11:40 +0100

    DEMAS-0 fe changes

--- frontend/app/data-enhancement/data-upload-form/data-upload-form.types.ts
+++ frontend/app/data-enhancement/data-upload-form/data-upload-form.types.ts
@@ -1,13 +1,18 @@
-export type JsonValue = string | number | boolean | null | JsonObject | JsonArray
-export type JsonObject = { [key: string]: JsonValue }
-export type JsonArray = JsonValue[]
+export type JsonValue =
+	| string
+	| number
+	| boolean
+	| null
+	| JsonObject
+	| JsonArray;
+export type JsonObject = { [key: string]: JsonValue };
+export type JsonArray = JsonValue[];
 
-export type ParsedData = Record<string, string | number | boolean | null>[]
+export type ParsedData = Record<string, string | number | boolean | null>[];
 
 export type FileUploadState = {
-  file: File | null
-  parsedData: ParsedData | null
-  error: string | null
-  isLoading: boolean
-}
-
+	file: File | null;
+	parsedData: ParsedData | null;
+	error: string | null;
+	isLoading: boolean;
+};
